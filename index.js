1cbb40a1c04923abd06bfe45c4dd606ceaea24cc
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2024-02-17 16:16:20 +0400

    expanding comments with click

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto('https://old.reddit.com/r/learnprogramming/comments/t2jaes/i_learned_to_code_in_2_months_and_got_a_remote/');
-  await page.setViewport({width: 1080, height: 1024});
+  await page.goto(
+    "https://old.reddit.com/r/learnprogramming/comments/t2jaes/i_learned_to_code_in_2_months_and_got_a_remote/"
+  );
+  await page.setViewport({ width: 1080, height: 1024 });
 
-  await page.waitForNetworkIdle()
+  let expandButtons = await page.$$(".morecomments");
 
+  console.log(expandButtons.length);
+
+  while (expandButtons.length) {
+    for (let btn of expandButtons) {
+      await btn.click();
+      await page.waitForNetworkIdle();
+    }
+
+    expandButtons = await page.$$(".morecomments");
+  }
 
   await browser.close();
 })();
